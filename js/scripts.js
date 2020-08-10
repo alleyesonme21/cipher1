6950ddf238270075b14b6f273f5802be223ac44f
jd <duverseaujohnny21@gmail.com>
2020-08-10 17:38:00 -0400

    add the third function
    Co-authored-by: Joseph Karnafel <josephkarnafel@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,35 @@
 const sentence = prompt("Enter a sentence");
 
+function reverseFirstAndLast(sentence) {
+  const step1 = firstAndLast(sentence);
+  const step2 = step1.toUpperCase();
+  console.log(step2);
+  const step3 = reverse(step2);
+  console.log(step3);
+  console.log(sentence + step3);
+}
+
 function firstAndLast(sentence) {
   return sentence.charAt(0) + sentence.charAt(sentence.length-1);
 }
+
 const result1 = firstAndLast(sentence).toUpperCase();
 
 function reverse(result1) {
-  return result1.charAt(1) + result1.charAt(0);
+return result1.charAt(1) + result1.charAt(0);
 }
-console.log(reverse(result1));
+
+reverseFirstAndLast(sentence);
+
+// console.log(reverse(result1));
+
+// function firstAndLast(sentence) {
+//   return sentence.charAt(0) + sentence.charAt(sentence.length-1);
+// }
+// const result1 = firstAndLast(sentence).toUpperCase();
+
+// function reverse(result1) {
+//   return result1.charAt(1) + result1.charAt(0);
+// }
+// console.log(reverse(result1));
 
